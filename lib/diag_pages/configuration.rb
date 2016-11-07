1495e987d79d525ba6602e71e201fa0626f35eaa
Jeremy Hicks <jeremy.hicks@pnmac.com>
2016-11-07 10:09:18 -0800

    Hide the default nslookup

--- lib/diag_pages/configuration.rb
+++ lib/diag_pages/configuration.rb
@@ -9,20 +9,20 @@ module DiagPages
     def initialize
       @diag_tests = []
       @default_tests = [
-        {
-          name: "nslookup",
-          link: "nslookup",
-          description: "It will lookup the IP Address",
-          status: nil,
-          block: lambda {
-            begin
-              @dnsr = Resolv::DNS.new(nameserver: ['8.8.8.8', '8.8.8.4'])
-              return @dnsr.getaddress "pennymacusa.com"
-            rescue Resolv::ResolvError => e
-              return "fail"
-            end
-          }
-        }
+        # {
+        #   name: "nslookup",
+        #   link: "nslookup",
+        #   description: "It will lookup the IP Address",
+        #   status: nil,
+        #   block: lambda {
+        #     begin
+        #       @dnsr = Resolv::DNS.new(nameserver: ['8.8.8.8', '8.8.8.4'])
+        #       return @dnsr.getaddress "pennymacusa.com"
+        #     rescue Resolv::ResolvError => e
+        #       return "fail"
+        #     end
+        #   }
+        # }
       ]
 
       @external_links = []
